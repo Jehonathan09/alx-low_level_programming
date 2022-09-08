1342682c4bb045af6d5a4f5a4cd6de3ac9469759
jehonathan09 <jonathanmwenge64@gmail.com>
2022-09-08 12:37:45 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main : prints some string to the console
+ * main - print string in printf function
  *
  * Description: print a string using printf
  * Return: Always 0 (Success)
