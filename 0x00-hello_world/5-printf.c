2424e215b357793d3df65210999a2aa98b62cf28
jehonathan09 <jonathanmwenge64@gmail.com>
2022-09-08 12:31:37 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
 /**
  * main : prints some string to the console
  *
+ * Description: using tha main function
+ * this program prints the string "with proper grammar, but the outcome is a piece of art,"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
