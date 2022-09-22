3b388acc4e680e87cd043eff1fb930473bbc93a3
jehonathan09 <jonathanmwenge64@gmail.com>
2022-09-22 15:41:44 -0700

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,18 @@
  */
 char leet(char *s)
 {
-	int i = 0, j;
-	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	while(s[i])
+
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	chars b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j < 10; j++)
-			if (s[i] == str[j])
-				s[i] = subs[j];
-		i++;
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
 	}
 	return (s);
 }
